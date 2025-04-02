da4ca376034002312d450563a8f1c33eb1d50cf2
hopin22 <sprzedawca705@gmail.com>
2025-04-02 14:10:35 +0000

    3_1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,19 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
-
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+    int wysokosc = 10;
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+    // Odwrócony trójkąt prostokątny
+    for (int i = 0; i < wysokosc; i++) {
+      // Spacje z lewej strony
+      for (int j = 0; j < i; j++) {
+        System.out.print(" ");
+      }
+      // Gwiazdki
+      for (int k = 0; k < (wysokosc - i); k++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
 }
+
